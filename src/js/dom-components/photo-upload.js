8aee071ddcdea91702cb0666e47607762f4a6679
Andrew Schorr <afs35mm@gmail.com>
2018-05-29 21:12:39 -0400

    adding images upload ish

--- src/js/dom-components/photo-upload.js
+++ src/js/dom-components/photo-upload.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Z_SYNC_FLUSH } from 'zlib';
 
 class PhotoUpload extends Component {
     constructor(props) {
@@ -13,25 +14,35 @@ class PhotoUpload extends Component {
         const pFiles = files.map(file => {
             const reader = new FileReader();
             const readerP = new Promise((resolve, reject) => {
-                reader.onloadend = resolve;
+                reader.onloadend = () => {
+                    resolve({ img: reader.result, file });
+                };
             });
             reader.readAsDataURL(file);
-            return readerP.then(e => {
-                const img = new Image();
-                new Promise((resolve, reject) => {
-                    img.onload = resolve;
-                    img.onerror = resolve;
-                });
-                img.src = e.target.result;
-                return img;
-            });
+            return readerP;
         });
-        Promise.all(pFiles).then(imgs => {
-            this.setState({ imgs });
+        Promise.all(pFiles).then(images => {
+            this.setState({ imgs: this.state.imgs.concat(images) });
         });
     }
 
     render() {
+        const imgs = this.state.imgs.length ? (
+            this.state.imgs.map((imgInfo, i) => (
+                <li key={i} className="item-holder">
+                    <img className="item-image" src={imgInfo.img} />
+                    <svg
+                        xlinkHref="http://www.w3.org/2000/svg"
+                        className="svg-icon"
+                        viewBox="0 0 20 20">
+                        <path d="M10.185,1.417c-4.741,0-8.583,3.842-8.583,8.583c0,4.74,3.842,8.582,8.583,8.582S18.768,14.74,18.768,10C18.768,5.259,14.926,1.417,10.185,1.417 M10.185,17.68c-4.235,0-7.679-3.445-7.679-7.68c0-4.235,3.444-7.679,7.679-7.679S17.864,5.765,17.864,10C17.864,14.234,14.42,17.68,10.185,17.68 M10.824,10l2.842-2.844c0.178-0.176,0.178-0.46,0-0.637c-0.177-0.178-0.461-0.178-0.637,0l-2.844,2.841L7.341,6.52c-0.176-0.178-0.46-0.178-0.637,0c-0.178,0.176-0.178,0.461,0,0.637L9.546,10l-2.841,2.844c-0.178,0.176-0.178,0.461,0,0.637c0.178,0.178,0.459,0.178,0.637,0l2.844-2.841l2.844,2.841c0.178,0.178,0.459,0.178,0.637,0c0.178-0.176,0.178-0.461,0-0.637L10.824,10z" />
+                    </svg>
+                </li>
+            ))
+        ) : (
+            <p className="no-photos">No images yet ️☹️, upload some! 😄</p>
+        );
+
         return (
             <div className="photo-uploader">
                 <div className="custom-file">
@@ -47,29 +58,7 @@ class PhotoUpload extends Component {
                         Choose file
                     </label>
                 </div>
-                <ul className="images-list">
-                    <li className="item-holder">
-                        <img className="item-image" src="http://via.placeholder.com/350x150" />
-                    </li>
-                    <li className="item-holder">
-                        <img className="item-image" src="http://via.placeholder.com/150x350" />
-                    </li>
-                    <li className="item-holder">
-                        <img className="item-image" src="http://via.placeholder.com/400x400" />
-                    </li>
-                    <li className="item-holder">
-                        <img className="item-image" src="http://via.placeholder.com/10x20" />
-                    </li>
-                    <li className="item-holder">
-                        <img className="item-image" src="http://via.placeholder.com/30x10" />
-                    </li>
-                    <li className="item-holder">
-                        <img className="item-image" src="http://via.placeholder.com/350x150" />
-                    </li>
-                    <li className="item-holder">
-                        <img className="item-image" src="http://via.placeholder.com/350x150" />
-                    </li>
-                </ul>
+                <ul className="images-list">{imgs}</ul>
             </div>
         );
     }
